661e979fd4ff69a4f8013babf4e5f7a7d279ae35
Marcos Queiroz <marcosmqueiroz@live.com>
2019-05-14 21:57:25 -0300

    Seção 8 - Aula 79

--- ProAgil.WebAPI/Dtos/EventoDto.cs
+++ ProAgil.WebAPI/Dtos/EventoDto.cs
@@ -1,21 +1,20 @@
-using System;
 using System.Collections.Generic;
 
-namespace ProAgil.Domain
+namespace ProAgil.WebAPI.Dtos
 {
-    public class Evento
+    public class EventoDto
     {
         public int Id { get; set; }
         public string Local { get; set; }
-        public DateTime DataEvento { get; set; }
+        public string DataEvento { get; set; }
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string ImagemURL { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
         public string Lote { get; set; }
-        public List<Lote> Lotes { get; set; }
-        public List<RedeSocial> RedesSociais { get; set; }
-        public List<PalestranteEvento> PalestrantesEventos { get; set; }
+        public List<LoteDto> Lotes { get; set; }
+        public List<RedeSocialDto> RedesSociais { get; set; }
+        public List<PalestranteDto> Palestrantes { get; set; }
     }
 }
